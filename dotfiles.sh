cd6a08b786fff531dda8188eb02c64056cedb80a
Jan K. Griffioen-Järfalk <jan@jarfalk.se>
2020-03-26 01:04:06 +0100

    adds dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,9 +1,11 @@
 # Remove current dotfiles
 rm "${HOME}/.zshrc" 2>/dev/null
-rm "${HOME}/.gitignore_global" 2>/dev/null
+rm "${HOME}/.gitignore" 2>/dev/null
 rm "${HOME}/.gitconfig" 2>/dev/null
+rm "${HOME}/.gitattributes" 2>/dev/null
 
 # Symlink to new dotfiles
 ln -s "${HOME}/dotfiles/.zshrc" "${HOME}/.zshrc"
 ln -s "${HOME}/dotfiles/.gitignore" "${HOME}/.gitignore"
 ln -s "${HOME}/dotfiles/.gitconfig" "${HOME}/.gitconfig"
+ln -s "${HOME}/dotfiles/.gitattributes" "${HOME}/.gitattributes"
