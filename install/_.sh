291c6d20f681e7cf54727c19ed10c0cb4e21d207
Jan K. Griffioen-Järfalk <jan@jarfalk.se>
2020-04-06 01:19:58 +0200

    adds stuff

--- install/_.sh
+++ install/_.sh
@@ -14,32 +14,31 @@ brew cask install spotify
 brew cask install 1password
 brew cask install kaleidoscope
 brew cask install slack
-
-# TODO
-# Charles
-# Screaming Frog
-# Postman?
+brew cask install charles08
+brew cask install screaming-frog-seo-spider
+brew cask install postman
 
 # Terminal
 brew cask install iterm2
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install git and the Github exenstions hub.
-brew install git hub
+# brew install git hub
+brew install github/gh/gh
 
 # Install node and node related things.
 source ./install/node.sh
 
+# Install App Store.
+source ./install/appStore.sh
+
+
 # Install global npm packages.
-npm install --global diff-so-fancy
+# npm install --global diff-so-fancy
 
 # Install Logitech Vertical MX drivers.
 # brew tap homebrew/cask-drivers
 # brew cask install logitech-options
 
-# Install App Store applications.
-brew install mas
-mas signin jan@jarfalk.se
-mas install 441258766 # Magnet
-mas install 1116599239 # NordVPN
+
 
